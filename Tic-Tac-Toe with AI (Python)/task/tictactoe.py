55f6dac06ba50c7db5b96855c31245cb1b10d0f0
Tom Verweij <tom.verweij@inergy.nl>
2023-05-11 00:00:41 +0200

    stage 4/5 done, PEP 8 complaint

--- Tic-Tac-Toe with AI (Python)/task/tictactoe.py
+++ Tic-Tac-Toe with AI (Python)/task/tictactoe.py
@@ -9,7 +9,6 @@ class TicTacToeBoard:
         self.board_list = [['_', '_', '_'],
                            ['_', '_', '_'],
                            ['_', '_', '_']]
-        print(self.get_board())
 
     def get_board(self):
         board = '\n---------\n'
@@ -19,30 +18,90 @@ class TicTacToeBoard:
         return board
 
     def accept_move(self, player):
-        while True:
-            if self.players[player] == 'user':
-                m = input('Enter the coordinates:')
-            else:  # later this will have more levels, now by default 'easy'
-                m = str(random.randrange(1, 4, 1)) + ' ' + str(random.randrange(1, 4, 1))
+        possible_moves = []
+        for x in range(3):
+            for y in range(3):
+                if self.board_list[x][y] == '_':
+                    possible_moves += [str(x + 1) + ' ' + str(y + 1)]
 
-            # check move input
-            try:
-                x = int(m.split()[0])
-                y = int(m.split()[1])
-                if not 1 <= x <= 3 or not 1 <= y <= 3:
-                    print('Coordinates should be from 1 to 3!')
-                    continue
-                elif self.board_list[x - 1][y - 1] != '_':
-                    if self.players[player] == 'user':
+        if self.players[player] == 'user':
+            while True:
+                m = input('Enter the coordinates:')
+
+                try:
+                    x = int(m.split()[0])
+                    y = int(m.split()[1])
+                    if not 1 <= x <= 3 or not 1 <= y <= 3:
+                        print('Coordinates should be from 1 to 3!')
+                        continue
+                    elif m not in possible_moves:
                         print('This cell is occupied! Choose another one!')
-                    continue
-                else:
-                    self.board_list[x - 1][y - 1] = player
-                    if self.players[player] == 'easy':
-                        print('Making move level "easy"')
-                    break
-            except ValueError:
-                print('You should enter numbers!')
+                        continue
+                    else:
+                        self.board_list[x - 1][y - 1] = player
+                        break
+                except (ValueError, IndexError):
+                    print('You should enter numbers!')
+
+        elif self.players[player] == 'easy':
+            m = random.choice(possible_moves)
+
+        else:  # 'medium'
+            win_move = self.find_win_move(player)
+            defense_move = self.find_win_move('X' if player == 'O' else 'O')
+            if win_move is not None:
+                m = win_move
+            elif defense_move is not None:
+                m = defense_move
+            else:  # play easy
+                m = random.choice(possible_moves)
+
+        if self.players[player] in ['easy', 'medium']:
+            print(f'Making move level "{self.players[player]}"')
+            x = int(m.split()[0])
+            y = int(m.split()[1])
+            self.board_list[x - 1][y - 1] = player
+
+    def find_win_move(self, player):
+        # check horizontal for possible win moves
+        x = 1
+        for row in self.board_list:
+            if row.count(player) == 2 and row.count('_') == 1:
+                # find the index (+1) for column (=y)
+                return str(x) + ' ' + str([y for y in range(3) if row[y] == '_'][0] + 1)
+            x += 1
+
+        # check vertical for possible win moves
+        cols = ['', '', '']
+        for x in range(3):
+            for y in range(3):
+                cols[y] += self.board_list[x][y]
+        y = 1
+        for col in cols:
+            if col.count(player) == 2 and col.count('_') == 1:
+                # find the index (+1) for row (=x)
+                return str([x for x in range(3) if col[x] == '_'][0] + 1) + ' ' + str(y)
+            y += 1
+
+        # check diagonal for possible win moves
+        diagonals = [['_', '_', '_'], ['_', '_', '_']]
+        diagonals[0] = self.board_list[0][0] + self.board_list[1][1] + self.board_list[2][2]
+        diagonals[1] = self.board_list[0][2] + self.board_list[1][1] + self.board_list[2][0]
+        if diagonals[0].count(player) == 2 and diagonals[0].count('_') == 1:
+            if diagonals[0][0] == '_':
+                return '1 1'
+            elif diagonals[1][1] == '_':
+                return '2 2'
+            else:
+                return '3 3'
+
+        if diagonals[1].count(player) == 2 and diagonals[1].count('_') == 1:
+            if diagonals[1][2] == '_':
+                return '1 3'
+            elif diagonals[1][1] == '_':
+                return '2 2'
+            else:
+                return '3 1'
 
     def is_win(self, xo):
         # check horizontal for a win of xo
@@ -96,18 +155,19 @@ class Menu:
                 if action[0] == 'exit':
                     break
 
-                valid_player_types = ['user', 'easy']
+                valid_player_types = ['user', 'easy', 'medium']
                 if action[0] == 'start' and set(action[1:2]).issubset(valid_player_types):
                     players = {'X': action[1], 'O': action[2]}
                     ttt = TicTacToeBoard(players)
+                    print(ttt.get_board())
                     next_player = 'X'
                     while ttt.get_score() == 'Game not finished':
                         ttt.accept_move(next_player)
+                        print(ttt.get_board())
                         if next_player == 'X':
                             next_player = 'O'
                         else:
                             next_player = 'X'
-                        print(ttt.get_board())
                     print(ttt.get_score())
             except IndexError:
                 print('Bad parameters!')
